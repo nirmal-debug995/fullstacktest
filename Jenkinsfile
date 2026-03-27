242ba836d273bf2950e67407e7d65b59155b0a48
Ubuntu <azureuser@fullstacktest-vm.d1gysty00ske3fm5ojmdvz04de.rx.internal.cloudapp.net>
2026-03-27 09:29:13 +0000

    Added CI/CD Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    tools {
+        nodejs "node16"
+    }
+
+    environment {
+        PORT = "3000"
+    }
+
+    stages {
+
+        stage('Clone Repo') {
+            steps {
+                git 'https://github.com/nirmal-debug995/fullstacktest.git'
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+
+        stage('Stop Old App') {
+            steps {
+                sh 'pkill -f "node app.js" || true'
+            }
+        }
+
+        stage('Run App') {
+            steps {
+                sh 'nohup npm start > app.log 2>&1 &'
+            }
+        }
+    }
+}
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import Express from "express";
 import http from "http";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import messageFormat from "./utils/messages.js";
 import * as userMethods from "./utils/users.js";
@@ -10,6 +10,9 @@ dotenv.config();
 const app = Express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+// ✅ FIXED PORT CONFIGURATION
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
 
 // set static folders
 app.use(Express.static("public"));
@@ -21,55 +24,46 @@ io.on("connection", socket => {
 
     // when user joined
     socket.on("user-join", ({ username, room }) => {
-        // create user object
         const user = userMethods.joinUser(socket.id, username, room);
 
-        // to specify a target room
         socket.join(user.room);
 
-        // when current user connects
         socket.emit("message", messageFormat(chatBotName, "Welcome to chat!"));
 
-        // when another user connects
         socket.broadcast
             .to(user.room)
             .emit("message", messageFormat(chatBotName, `${username} has Joined the chat!`));
 
-        // send room name & room users
-        io.to(user.room).emit("room-users", { room: user.room, users: userMethods.roomUsers(user.room) });
+        io.to(user.room).emit("room-users", {
+            room: user.room,
+            users: userMethods.roomUsers(user.room)
+        });
     });
-
 
     // when chat message received
     socket.on("chat-message", (msg) => {
-
-        // get current user info
         const user = userMethods.getCurrentUser(socket.id);
-
-        io.to(user.room).emit("message", messageFormat(user.username, msg));
-
+        if (user) {
+            io.to(user.room).emit("message", messageFormat(user.username, msg));
+        }
     });
 
     // when a user disconnects
     socket.on("disconnect", () => {
-
-        // get current left user info
         const user = userMethods.leaveUser(socket.id);
 
         if (user) {
             io.to(user.room).emit("message", messageFormat(chatBotName, `${user.username} has Left the chat!`));
+
+            io.to(user.room).emit("room-users", {
+                room: user.room,
+                users: userMethods.roomUsers(user.room)
+            });
         }
-
-        // send room name & room users
-        io.to(user.room).emit("room-users", { room: user.room, users: userMethods.roomUsers(user.room) });
     });
-
 });
 
-try {
-    const port = parseInt(process.env.SERVER_PORT);
-    server.listen(port, console.log(`server running on port ${port}!`));
-
-} catch (error) {
-    console.log(error);
-}
+// ✅ SAFE SERVER START
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
